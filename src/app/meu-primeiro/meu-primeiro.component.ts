4951f6007c92b3cd2703ce66f4b1a8c1fbbe8422
RenathaMachado <renatharenascer@icloud.com>
2020-09-21 22:21:17 -0300

    Criando componente
    Primeiro componente manual

---
+++ src/app/meu-primeiro/meu-primeiro.component.ts
@@ -0,0 +1,12 @@
+
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'meu-primeiro-component',  // Se torna uma tag para ser chamado em qualquer outro local
+    template: `<p> Meu primeiro component com Angular 2! </p> `
+    
+})
+ 
+export class MeuPrimeiroComponent{
+
+}
---
+++ src/app/meu-primeiro2/meu-primeiro2.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-meu-primeiro2',
+  templateUrl: './meu-primeiro2.component.html',
+  styleUrls: ['./meu-primeiro2.component.styl']
+})
+export class MeuPrimeiro2Component  {
+
+ 
+
+}
